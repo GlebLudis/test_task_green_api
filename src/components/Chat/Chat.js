047c87bf76b7c0c9202a54341d308acad451dbd4
Gleb Ludis <glebludis@yandex.ru>
2023-05-14 15:55:51 +0300

    add basic functional in Chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Chat.css"
 import {Avatar} from "@mui/material";
 
 function Chat() {
+
+    const [input, setInput] = useState("")
+
+    const sendMessage = (e)  => {
+        e.preventDefault();
+        console.log(input)
+
+    }
+
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -14,14 +24,19 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className={`chat__message $ { true && 'chat__reciever'}`}>
+                <p className={`chat__message ${ true && 'chat__reciever'}`}>
                     <span className="chat__name"> Иван Иванов</span>
                     Hello, world!
                     <span className="chat__timestamp">15:00</span>
                 </p>
 
             </div>
-            <div className="chat__footer"></div>
+            <div className="chat__footer">
+                <form>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Сообщение..."/>
+                    <button onClick={sendMessage}type="submit">Отправить сообщение</button>
+                </form>
+            </div>
         </div>
     )
 }
